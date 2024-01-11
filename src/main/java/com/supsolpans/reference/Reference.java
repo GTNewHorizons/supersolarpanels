5153c48bda780f278c4043aefb728296ef39bbcf
Martin Robertz <dream-master@gmx.net>
2024-01-11 20:08:32 +0100

    update

--- src/main/java/com/supsolpans/reference/Reference.java
+++ src/main/java/com/supsolpans/reference/Reference.java
@@ -4,6 +4,6 @@ public class Reference {
 
     public static final String MOD_ID = "supersolarpanel";
     public static final String MOD_NAME = "Super Solar Panel";
-    public static final String MOD_VERSION = "1.7.10-1.1.1-GT-NH-Mod";
+    public static final String MOD_VERSION = "GRADLETOKEN_VERSION";
     public static final String TEXTURES_BLOCKS = MOD_ID + ":textures/blocks/";
 }
